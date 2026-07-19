d23efcfde2e3fcc9b6a7c39d8aa9615e4dd8754c
dimon27254 <55013272+dimon27254@users.noreply.github.com>
2026-07-19 22:12:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ RED_COLOR="\033[1;31m"
 GREEN_COLOR="\033[1;32m"
 BOLD_TEXT="\033[1m"
 NO_STYLE="\033[0m"
-CRONTABS_DIR="/opt/var/spool/cron/crontabs"
 antiscan_string="$(opkg list-installed antiscan)"
 REPO_URL="https://dimon27254.github.io/antiscan/all"
 
@@ -26,25 +25,6 @@ print_message() {
   esac
 }
 
-cron_installed() {
-  cron_string="$(opkg list-installed cron)"
-  crond_busybox_string="$(opkg list-installed crond-busybox)"
-  if [ -z "$cron_string" ] && [ -z "$crond_busybox_string" ]; then
-    cron_init_scripts="$(find /opt/etc/init.d -regex '.*\/[sS][0-9]+cron.*')"
-    if [ -z "$cron_init_scripts" ]; then
-      return 1
-    else
-      if [ ! -d "$CRONTABS_DIR" ]; then
-        return 1
-      else
-        return 0
-      fi
-    fi
-  else
-    return 0
-  fi
-}
-
 print_message "notice" "Устанавливаем пакеты для доступа к репозиторию Antiscan..."
 if opkg update && opkg install wget-ssl ca-bundle; then
   print_message "success" "wget-ssl и ca-bundle успешно установлены"
@@ -52,41 +32,12 @@ if opkg update && opkg install wget-ssl ca-bundle; then
   mkdir -p /opt/etc/opkg
   echo "src/gz antiscan ${REPO_URL}" >"/opt/etc/opkg/antiscan.conf"
   if opkg update; then
-    print_message "notice" "Проверяем наличие cron..."
-    if ! cron_installed; then
-      print_message "notice" "Cron не найден. Устанавливаем..."
-      if opkg install cron; then
-        print_message "success" "Cron успешно установлен"
-        print_message "notice" "Настраиваем cron..."
-        if [ ! -d "$CRONTABS_DIR" ]; then
-          mkdir -p "$CRONTABS_DIR"
-        fi
-        if ! crontab -l >/dev/null 2>&1; then
-          echo | crontab -
-        fi
-        sed -i 's/="-s"/=""/' "/opt/etc/init.d/S10cron"
-        if "/opt/etc/init.d/S10cron" start; then
-          print_message "success" "Cron готов к работе"
-          print_message "notice" "Переходим к установке Antiscan..."
-        else
-          print_message "error" "Не удалось запустить cron"
-          return 1
-        fi
-      else
-        print_message "error" "Не удалось установить cron"
-        return 1
-      fi
-    else
-      print_message "notice" "Cron найден. Переходим к установке Antiscan..."
-    fi
-
     if opkg install antiscan --force-reinstall; then
       if [ -z "$antiscan_string" ]; then
         print_message "success" "Antiscan успешно установлен!"
-        print_message "notice" "Выполните настройку в ascn.conf и запустите Antiscan."
+        print_message "notice" "Выполните настройку в ascn.conf и перезапустите Antiscan."
       else
         print_message "success" "Antiscan успешно обновлен!"
-        print_message "notice" "Не забудьте его запустить."
       fi
     else
       print_message "error" "Не удалось установить Antiscan"
